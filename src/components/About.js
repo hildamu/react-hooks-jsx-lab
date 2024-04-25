8a3d14eeff50ccc07c5f46c389ea78512b752a2f
hildamu <mukamihilda437@gmail.com>
2024-04-25 08:01:21 +0300

    submit

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { image } from "../data/data";
+//import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div>
+    <div id="about">
+    <h2>About Me</h2>
+    <p>I am a champ!</p>
+    <h2>a</h2>
+    </div>
+
+    <img src ="https://i.imgur.com/mV8PQxj.gif" alt ="I made this"></img>
+  </div>;
 }
 
 export default About;
